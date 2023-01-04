f0a3a2a6d41c0b0260da7ce8a9a046feef53de47
isabellexvr <isabellexavier59@gmail.com>
2023-01-03 23:43:53 -0300

    feat: find all links controller

--- src/controllers/posts.controllers.js
+++ src/controllers/posts.controllers.js
@@ -9,6 +9,23 @@ import dayjs from "dayjs";
     createdAt
 } */
 
+export async function publicateLink(req, res) {
+  const { url, description } = req.body;
+  const userId = res.locals.userId;
+  try {
+    const now = `${dayjs().year()}/${
+      dayjs().month().length + 1 < 2
+        ? dayjs().month() + 1
+        : "0" + (dayjs().month() + 1)
+    }/${dayjs().date().length < 2 ? dayjs().date() : "0" + dayjs().date()}`;
+    /*     await connection.query(`INSERT INTO posts ("userId", url, description, "createdAt") VALUES $1, $2, $3, $4;`, [userId, url, description, now]) */
+    res.status(201).send("Post criado com sucesso!");
+  } catch (err) {
+    res.status(500).send(err.message);
+    console.log(err.message);
+  }
+}
+
 /* urlMetadata(url)
 .then((answer) => {
   return res.status(201).send({
@@ -26,17 +43,41 @@ import dayjs from "dayjs";
   console.log(err);
 }); */
 
-export async function publicateLink(req, res) {
-  const { url, description } = req.body;
+export async function findAllLinks(req, res) {
   const userId = res.locals.userId;
   try {
-    const now = `${dayjs().year()}/${
-      dayjs().month().length + 1 < 2
-        ? dayjs().month() + 1
-        : "0" + (dayjs().month() + 1)
-    }/${dayjs().date().length < 2 ? dayjs().date() : "0" + dayjs().date()}`;
-    /*     await connection.query(`INSERT INTO posts ("userId", url, description, "createdAt") VALUES $1, $2, $3, $4;`, [userId, url, description, now]) */
-    res.status(201).send("Post criado com sucesso!");
+    const { rows } = connection.query(
+      `SELECT 
+        users.name AS "userName",
+        posts.description, 
+        posts.url, 
+        posts."createdAt" 
+      FROM 
+        posts 
+      JOIN 
+        users 
+      ON 
+       users.id=posts."userId" 
+      WHERE 
+       posts."userId"=$1;`,
+      [userId]
+    );
+    urlMetadata(rows[0].url)
+      .then((answer) => {
+        return res.status(201).send({
+          userName: rows[0].userName,
+          postDescription: rows[0].description,
+          linkInfo: {
+            title: answer.title,
+            description: answer.description,
+            url: answer.url,
+            image: answer.image,
+          },
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   } catch (err) {
     res.status(500).send(err.message);
     console.log(err.message);
