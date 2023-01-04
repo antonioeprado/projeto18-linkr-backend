df76a1dad0c574d449aecf24721b735b37956a68
Antonio Prado <antonioeprado18@gmail.com>
2023-01-03 22:26:20 -0300

    refactor: queries extracted to User repository

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,56 +1,50 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-import connection from "../database/db.js";
-
-export async function postSignUp(req, res) {
-  const { email, password, username, pictureUrl } = req.body;
-
-  const passwordHashed = bcrypt.hashSync(password, 10);
-
-  try {
-    const existingEmail = await connection.query(
-      `SELECT * FROM users WHERE email = $1`,
-      [email]
-    );
-
-    if (existingEmail.rowCount > 0) {
-      return res
-        .status(409)
-        .send("Esse e-mail já está cadastrado no nosso sistema!");
-    }
-
-    const existingUsername = await connection.query(
-      `SELECT * FROM users WHERE username = $1`,
-      [username]
-    );
-
-    if (existingUsername.rowCount > 0) {
-      return res
-        .status(409)
-        .send("Esse nome de usuário já está cadastrado no nosso sistema!");
-    }
-
-    const newUser = await connection.query(
-      `INSERT INTO users (email, password, username, "pictureUrl") VALUES ($1, $2, $3, $4)`,
-      [email, passwordHashed, username, pictureUrl]
-    );
-    res.sendStatus(201);
-  } catch (err) {
-    console.log(err);
-  }
-}
-
 import { User } from "../repositories/auth.repository.js";
 
+export async function postSignUp(req, res) {
+	const { email, password, username, pictureUrl } = req.body;
+
+	const passwordHashed = bcrypt.hashSync(password, 10);
+
+	try {
+		const existingEmail = await User.findByEmail(email);
+
+		if (existingEmail.rowCount > 0) {
+			return res
+				.status(409)
+				.send("Esse e-mail já está cadastrado no nosso sistema!");
+		}
+
+		const existingUsername = await User.findByUsername(username);
+
+		if (existingUsername.rowCount > 0) {
+			return res
+				.status(409)
+				.send("Esse nome de usuário já está cadastrado no nosso sistema!");
+		}
+
+		await User.createUser({
+			email,
+			password: passwordHashed,
+			username,
+			pictureUrl,
+		});
+		res.sendStatus(201);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
 export async function userById(req, res) {
-  const { id } = req.params;
-  try {
-    const query = await User.findById(id);
-    const user = query.rows;
-    res.status(200).send(user);
-  } catch (error) {
-    console.log(`Error trying to find user with id: ${id}`);
-    console.log(`Server returned: ${error}`);
-    res.sendStatus(500);
-  }
+	const { id } = req.params;
+	try {
+		const query = await User.findById(id);
+		const user = query.rows;
+		res.status(200).send(user);
+	} catch (error) {
+		console.log(`Error trying to find user with id: ${id}`);
+		console.log(`Server returned: ${error}`);
+		res.sendStatus(500);
+	}
 }
--- src/repositories/auth.repository.js
+++ src/repositories/auth.repository.js
@@ -1,15 +1,15 @@
 import connection from "../database/db.js";
 
 export const User = {
-  findById: function (id) {
-    // A FUNÇÃO JSON_BUILD_OBJECT monta um objeto da seguinte forma:
-    // 'nomedachave', nomedocampo
-    // A FUNÇÃO ARRAY_AGG transforma esses objetos em uma array em SQL
-    // que retorna uma array com os valores dos campos.
-    // A FUNÇÃO ARRAY_TO_JSON pega essa array e transforma em uma array
-    // de objetos.
-    return connection.query(
-      `
+	findById: function (id) {
+		// A FUNÇÃO JSON_BUILD_OBJECT monta um objeto da seguinte forma:
+		// 'nomedachave', nomedocampo
+		// A FUNÇÃO ARRAY_AGG transforma esses objetos em uma array em SQL
+		// que retorna uma array com os valores dos campos.
+		// A FUNÇÃO ARRAY_TO_JSON pega essa array e transforma em uma array
+		// de objetos.
+		return connection.query(
+			`
 			SELECT
 				u.name,
 				ARRAY_TO_JSON(
@@ -27,7 +27,21 @@ export const User = {
 			WHERE id=$1
 			GROUP BY u.id
 			`,
-      [id]
-    );
-  },
+			[id]
+		);
+	},
+	findByEmail: function (email) {
+		return connection.query(`SELECT * FROM users WHERE email = $1`, [email]);
+	},
+	findByUsername: function (username) {
+		return connection.query(`SELECT * FROM users WHERE username = $1`, [
+			username,
+		]);
+	},
+	createUser: function (obj) {
+		return connection.query(
+			`INSERT INTO users (email, password, username, "pictureUrl") VALUES ($1, $2, $3, $4)`,
+			[obj.email, obj.password, obj.username, obj.pictureUrl]
+		);
+	},
 };
