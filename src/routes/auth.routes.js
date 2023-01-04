84ab0ecb0e6099bf99a31155ca2f44b0596c670b
Antonio Prado <antonioeprado18@gmail.com>
2023-01-04 15:09:17 -0300

    feat: added search route for frontend searchbox

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { postSignUp } from "../controllers/auth.controllers.js";
+import { postSignUp, userByName } from "../controllers/auth.controllers.js";
 import { signUpSchemaValidation } from "../middlewares/auth.validation.middleware.js";
 
 const router = Router();
 
 router.post("/signup", signUpSchemaValidation, postSignUp);
+
+router.post("/user", userByName);
 
 export default router;
