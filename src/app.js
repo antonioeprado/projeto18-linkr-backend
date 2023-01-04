955e20e1f78d1ce0648b42e173892395a6699b6d
RodolphoBuettel <Rodolpho05buettel@gmail.com>
2023-01-04 12:21:48 -0300

    feat: route hastags

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import hashtagsRoutes from "./routes/hashtags.routes.js";
+import cors from "cors";
 dotenv.config();
 
+
 const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use(hashtagsRoutes);
 
 const PORT = 4000 || process.env.PORT;
 app.listen(PORT, () => {
