a67031b189d3e3f907f96652ec0e7c515a0e8fb5
isabellexvr <isabellexavier59@gmail.com>
2023-01-03 23:44:45 -0300

    feat: find all links route

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { publicateLink } from "../controllers/posts.controllers.js";
+import {
+  publicateLink,
+  findAllLinks,
+} from "../controllers/posts.controllers.js";
 import postModelValidation from "../middlewares/post.model.validation.middleware.js";
 import tokenValidation from "../middlewares/token.validation.middleware.js";
 
 const postsRouter = Router();
 
 postsRouter.post("/post", postModelValidation, tokenValidation, publicateLink);
+
+postsRouter.get("/posts", tokenValidation, findAllLinks);
 
 export default postsRouter;
