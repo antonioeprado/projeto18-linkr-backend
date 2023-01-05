fd9c0cd03c769830b8cdc9855e37619ad875c018
Isabella Lopes <icmlopes@gmail.com>
2023-01-05 10:46:18 -0300

    feat: adding session id in the token and changing name of token authentication middleware

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import {
-	getUserById,
-	postSignIn,
-	postSignUp,
+  getUserById,
+  postSignIn,
+  postSignUp,
 } from "../controllers/auth.controllers.js";
 import {
-	ensureAuthenticated,
-	signInSchemaValidation,
-	signUpSchemaValidation,
+  ensureAuthentication,
+  signInSchemaValidation,
+  signUpSchemaValidation,
 } from "../middlewares/auth.validation.middleware.js";
 
 const router = Router();
@@ -15,6 +15,6 @@ const router = Router();
 router.post("/sign-up", signUpSchemaValidation, postSignUp);
 router.post("/sign-in", signInSchemaValidation, postSignIn);
 
-router.get("/user/:id", ensureAuthenticated, getUserById);
+router.get("/user/:id", ensureAuthentication, getUserById);
 
 export default router;
