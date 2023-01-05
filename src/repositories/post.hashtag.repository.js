43c25b028d60d1e6a2a9b43f0f4a4209123a22d7
isabellexvr <isabellexavier59@gmail.com>
2023-01-05 13:34:33 -0300

    fix: post hashtag query

--- src/repositories/post.hashtag.repository.js
+++ src/repositories/post.hashtag.repository.js
@@ -2,7 +2,7 @@ import connection from "../database/db.js";
 
 export default function postHashtag(tag) {
   return connection.query(
-    `INSERT INTO hashtags (tag, "createdAt") VALUES ($1);`,
+    `INSERT INTO hashtags (tag) VALUES ($1);`,
     [tag]
   );
 }
