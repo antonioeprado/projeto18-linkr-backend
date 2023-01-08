7d92a7854bad4c106c5b77223574a9f692eb3b83
isabellexvr <isabellexavier59@gmail.com>
2023-01-08 11:58:40 -0300

    fix: find all posts controllers res change + refac

--- src/controllers/likes.controllers.js
+++ src/controllers/likes.controllers.js
@@ -4,7 +4,6 @@ export async function postLike(req, res) {
   //postId e userId
   const { userId } = res.locals.user;
   const { postId } = req.params;
-  console.log("oi")
   try {
     await connection.query(
       `INSERT INTO likes ("postId","userId") VALUES ($1,$2);`,
@@ -21,7 +20,6 @@ export async function postLike(req, res) {
 export async function deleteLike(req, res) {
   const { userId } = res.locals.user;
   const { postId } = req.params;
-  console.log(userId, postId)
   try {
     await connection.query(
       `DELETE FROM likes WHERE "postId"=$1 AND "userId"=$2`,
