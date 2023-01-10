6b0567a13da7680313852addeac58102d36d6b39
Antonio Prado <antonioeprado18@gmail.com>
2023-01-09 22:13:35 -0300

    fix: changed controller function name to avoid mismatch

--- src/routes/follow.routes.js
+++ src/routes/follow.routes.js
@@ -1,12 +1,25 @@
 import { Router } from "express";
-import { followUser, unFollowUser } from "../controllers/follow.controllers.js";
+import {
+  followUserController,
+  unFollowUserController,
+} from "../controllers/follow.controllers.js";
 import { ensureAuthentication } from "../middlewares/auth.validation.middleware.js";
 import { isFollowing } from "../middlewares/follow.middlewares.js";
 
 const router = Router();
 
-router.post("/follow/:id", ensureAuthentication, isFollowing, followUser);
+router.post(
+  "/follow/:id",
+  ensureAuthentication,
+  isFollowing,
+  followUserController
+);
 
-router.delete("/unfollow/:id", ensureAuthentication, isFollowing, unFollowUser);
+router.delete(
+  "/unfollow/:id",
+  ensureAuthentication,
+  isFollowing,
+  unFollowUserController
+);
 
 export default router;
